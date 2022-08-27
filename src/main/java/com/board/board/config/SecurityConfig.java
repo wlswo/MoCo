e50f2f91c2272f069eabf71f52fd6496f3c9fb30
JaeJinByun <byunjin11@naver.com>
2022-08-28 01:32:15 +0900

    [UPDATE] 글수정 경로 인증받은사람만 접근 가능추가

--- src/main/java/com/board/board/config/SecurityConfig.java
+++ src/main/java/com/board/board/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig{
                     .antMatchers("/login").permitAll()
                     .antMatchers("/", "/css/**", "/img/**", "/js/**", "/h2/**", "/h2-console/**", "/favicon.ico").permitAll()
                     .antMatchers("/notice").permitAll()
-                    .antMatchers("/board/post").authenticated()
+                    .antMatchers("/board/post/**").authenticated()
                     .antMatchers("/board/**").permitAll()
                     .antMatchers("/signup","/login/signup").permitAll()
                     .antMatchers("/id/check","/name/check").permitAll()
