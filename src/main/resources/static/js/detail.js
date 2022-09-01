137bd220052ea22036f4abe8feeb67a83198dfee
JaeJinByun <byunjin11@naver.com>
2022-09-01 22:24:48 +0900

    [UPDATE] 댓글 좋아요 기능 통합

---
+++ src/main/resources/static/js/detail.js
@@ -0,0 +1,223 @@
+/* 좋아요 */
+
+function plzLogin() {
+    alert("로그인후 이용해주세요.");
+}
+
+/* CREATE */
+function likeCheck(boardId) {
+    /* isLiked == 0 좋아요를 누르지않은상태
+     * isLiked == 1 좋아요를 누른상태
+     */
+    const isClick = document.getElementById('isClick').value;
+    console.log(isClick);
+
+    /* CREATE */
+    if(isClick === 'noclick'){
+        /* ajax */
+        const baseUrl = "http://localhost:8080";
+        /* XMLHttpRequest 객체 정의 */
+        httpRequest = new XMLHttpRequest();
+
+        /* POST 방식으로 요청 */
+        httpRequest.open('POST', baseUrl + "/board/post/"+boardId+"/like");
+
+        /* ResponseType Json */
+        httpRequest.responseType = "json";
+
+        /* 정의된 서버에 Json 형식의 요청 Data를 포함하여 요청을 전송 */
+        httpRequest.send();
+
+        /* httpRequest 상태 감지 */
+        httpRequest.onreadystatechange = () => {
+            /* readyState가 Done이고 응답 값이 200(ok) 일때 받아온 boolean으로 분기 */
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    document.getElementById('isClick').value = 'clicked'
+                   /* window.location.reload(); */
+                } else {
+                    let error = httpRequest.response;
+                    console.log(error.message);
+                }
+            }
+
+        }
+    }
+    /* DELETE */
+    else {
+        /* ajax */
+        const baseUrl = "http://localhost:8080";
+        /* XMLHttpRequest 객체 정의 */
+        httpRequest = new XMLHttpRequest();
+
+        /* DELETE 방식으로 요청 */
+        httpRequest.open('DELETE', baseUrl + "/board/post/"+boardId+"/like");
+
+        /* ResponseType Json */
+        httpRequest.responseType = "json";
+
+        /* 정의된 서버에 Json 형식의 요청 Data를 포함하여 요청을 전송 */
+        httpRequest.send();
+
+        /* httpRequest 상태 감지 */
+        httpRequest.onreadystatechange = () => {
+            /* readyState가 Done이고 응답 값이 200(ok) 일때 받아온 boolean으로 분기 */
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    document.getElementById('isClick').value = 'noclick';
+                } else {
+                    let error = httpRequest.response;
+                    console.log(error.message);
+                }
+            }
+
+        }
+    }
+}
+
+
+
+/* 댓글 */
+
+/* CREATE */
+function saveComment(){
+    /* 게시글 번호 */
+    const boardId = document.getElementById("boardId").value;
+    /* 댓글 내용 */
+    const comment = document.getElementById("comment").value;
+
+    /* 공백 및 빈 문자열 체크 */
+    if(!comment || comment.trim() === "") {
+        alert("공백 또는 빈 문자열은 입력하실수 없습니다.");
+        return false;
+    }else {
+        /* ajax */
+        const baseUrl = "http://localhost:8080";
+        /* XMLHttpRequest 객체 정의 */
+        httpRequest = new XMLHttpRequest();
+
+        /* 입력된 데이터 Json 형식으로 변경 */
+        var reqJson = new Object();
+        reqJson.comment = comment;
+        reqJson.boardId = boardId;
+
+        /* POST 방식으로 요청 */
+        httpRequest.open('POST', baseUrl+"/board/comment/"+boardId);
+        /* 요청 Header에 컨텐츠 타입은 Json으로 사전 정의 */
+        httpRequest.setRequestHeader('Content-Type', 'application/json');
+        /* ResponseType Json */
+        httpRequest.responseType = "json";
+
+        /* 정의된 서버에 Json 형식의 요청 Data를 포함하여 요청을 전송 */
+        httpRequest.send(JSON.stringify(reqJson));
+
+        /* httpRequest 상태 감지 */
+        httpRequest.onreadystatechange = () => {
+            /* readyState가 Done이고 응답 값이 200(ok) 일때 받아온 boolean으로 분기 */
+            if(httpRequest.readyState === XMLHttpRequest.DONE) {
+                if(httpRequest.status === 200) {
+                    let result = httpRequest.response;
+                    console.log(result)
+                    alert("댓글이 등록되었습니다.");
+                    window.location.reload();
+                }else{
+                    let error = httpRequest.response;
+                    console.log(error.message);
+                }
+            }
+        }
+    }
+}
+/* 이벤트 부여 */
+document.querySelectorAll("#btn-comment-update").forEach(function (item){
+    item.addEventListener("click",function (){
+        const form = this.closest('form'); /* btn의 가장 가까운 조상의 Element(form)을 반환(closet) */
+        console.log(form);
+        commentUpdate(form);
+    });
+});
+
+/* UPDATE */
+function commentUpdate(form) {
+    /* json data */
+    const data = {
+        commentId : form.querySelector("#commentId").value,
+        boardId : form.querySelector("#comment_boardId").value,
+        comment : form.querySelector("#comment-content").value,
+    }
+
+    if(!data.comment || data.comment.trim() === ""){
+        alert("댓글을 입력해주세요.");
+        return false;
+    }
+
+    const comment_confirm = confirm("수정하시겠습니까?");
+
+    if (comment_confirm) {
+        /* ajax */
+        const baseUrl = "http://localhost:8080";
+        /* XMLHttpRequest 객체 정의 */
+        httpRequest = new XMLHttpRequest();
+
+        /* POST 방식으로 요청 */
+        httpRequest.open('PUT', baseUrl+"/board/post/"+ data.boardId +"/comment/" + data.commentId);
+        /* 요청 Header에 컨텐츠 타입은 Json으로 사전 정의 */
+        httpRequest.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        /* ResponseType Json */
+        httpRequest.responseType = "json";
+
+        /* 정의된 서버에 Json 형식의 요청 Data를 포함하여 요청을 전송 */
+        httpRequest.send(JSON.stringify(data));
+
+        /* httpRequest 상태 감지 */
+        httpRequest.onreadystatechange = () => {
+            /* readyState가 Done이고 응답 값이 200(ok) 일때 받아온 boolean으로 분기 */
+            if(httpRequest.readyState === XMLHttpRequest.DONE) {
+                if(httpRequest.status === 200) {
+                    window.location.reload();
+                }else{
+                    let error = httpRequest.response;
+                    console.log(error.message);
+                }
+            }
+        }
+    }
+}
+
+/* DELETE */
+function commentDelete(boardId, commentId) {
+    const comment_confirm = confirm("정말 삭제하시겠습니까?");
+
+    if(comment_confirm) {
+        /* ajax */
+        const baseUrl = "http://localhost:8080";
+        /* XMLHttpRequest 객체 정의 */
+        httpRequest = new XMLHttpRequest();
+
+        /* POST 방식으로 요청 */
+        httpRequest.open('DELETE', baseUrl + "/board/post/" + boardId + "/comment/" + commentId);
+        /* 요청 Header에 컨텐츠 타입은 Json으로 사전 정의 */
+        httpRequest.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        /* ResponseType Json */
+        httpRequest.responseType = "json";
+
+        /* 정의된 서버에 Json 형식의 요청 Data를 포함하여 요청을 전송 */
+        httpRequest.send();
+
+        /* httpRequest 상태 감지 */
+        httpRequest.onreadystatechange = () => {
+            /* readyState가 Done이고 응답 값이 200(ok) 일때 받아온 boolean으로 분기 */
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    window.location.reload();
+                } else {
+                    let error = httpRequest.response;
+                    console.log(error.message);
+                }
+            }
+
+        }
+    }
+}
+
+
