8dc3aaf182b01991f17853af5c36a456d21aa4c5
JaeJinByun <byunjin11@naver.com>
2022-09-09 03:41:59 +0900

    [NEED] 프론트쪽에서 메타마스크와 이더리움 테스트넷 기본 기능및 연동 필요

--- src/main/resources/static/js/web3js/web3js.js
+++ src/main/resources/static/js/web3js/web3js.js
@@ -1,3 +1,5 @@
+let currentAccount ;
+
 /* Web3가 브라우저에 주입되었는지 확인(Mist/MetaMask) */
 if (typeof web3 !== 'undefined') {
     /* Mist/MetaMask의 프로바이더 사용 */
@@ -6,26 +8,24 @@ if (typeof web3 !== 'undefined') {
 } else {
     /* 사용자가 Metamask를 설치하지 않은 경우에 대해 처리 */
     /* 사용자들에게 Metamask를 설치하라는 등의 메세지를 보여줄 것 */
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+    web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io"));
 }
 
+/* 메타마스크 연결 */
+document.getElementById("ConnectMetaMask").addEventListener("click", async function (event){
+    currentAccount = await web3.eth.requestAccounts().then(function(array) { return array[0] });
+    console.log(currentAccount);
+});
+/* smart Contract call */
+let testAbi = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "approve", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" } ], "name": "decreaseAllowance", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" } ], "name": "increaseAllowance", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "receiver", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transfer", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokens", "type": "uint256" } ], "name": "transferToken", "outputs": [ { "internalType": "bool", "name": "success", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "tokens", "type": "uint256" } ], "name": "TransferToken", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" } ], "name": "allowance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "balances", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [ { "internalType": "uint8", "name": "", "type": "uint8" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ];
+let contractAddress = "0x684c3e2310D88E18D7Dc237Ef745158cC7d0e7a5";
+document.getElementById("smartContract").addEventListener("click", async function (event){
+    const test_contract = new web3.eth.Contract(testAbi, contractAddress);
+    test_contract.defaultChain = 'goerli';
+    //토큰 개수
+    let amount = Web3js.utils.toHex(Web3js.utils.toWei("100")); //1 DEMO Token
 
-/* 사용자가 사용 중 이라고 브라우저가 인식하는 계정 */
-let userAccount;
-
-let checkAccountChange = setInterval(async function() {
-    /* 계정이 바뀌었는지 확인 */
-    let currentAccount = await web3.eth.requestAccounts().then(function(array) { return array[0] });
-    /* 현재 유저가 들고있는 계정(currentAccount)가 브라우저가 인식하는 계정(userAccount)와 다르다면 */
-    if (currentAccount !== userAccount) {
-        /* 계정을 업데이트 해준다 */
-        userAccount = currentAccount;
-        /* 새 계정에 대한 UI로 업데이트하기 위한 함수 호출 및 메시지 알림 */
-        console.log('Your account is ' + userAccount);
-    }
-}, 1000);     /* 1초 마다 계정 확인 */
-
-document.getElementById('smartContract').addEventListener('click', function (event){
-    const MyContract = new web3.eth.Contract(abi,"0x6e9a0D8C772e9b616dD277798e09A0a6122c56BC");
-    MyContract.method.HelloWorld().call().then(result => console.log("SmartContract Call : " + result));
+    //    const result = await test_contract.methods.mint(currentAccount,10).send({from:currentAccount});
+    const result = await test_contract.methods.mint("0x3764D8e80BB0CbfAA9B4bB6973EdeE0494d2D1eb", amount).send({from:currentAccount});
+    console.log(result);
 });
