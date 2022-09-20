309265a9db13445328505dbd9211437c486ed82b
JaeJinByun <byunjin11@naver.com>
2022-09-21 01:45:15 +0900

    [UPDATE] 해시태그 처리 추가

--- src/main/resources/static/js/board/list.js
+++ src/main/resources/static/js/board/list.js
@@ -31,17 +31,24 @@ function nextPage(){
                 let success = httpRequest.response;
                 success.forEach((item,index) => {
                     /* 구조분해할당 */
-                    let {id,title,writer,content,thumbnail,subcontent,view,createdDate,modifiedDate,usetId,userImg,comments,likes} = item;
+                    let {id,title,writer,thumbnail,subcontent,view,created_date,user_id,picture,comment_count,like_count,hashTag} = item;
                     /* 날짜 포맷 변경 */
-                    CustomDate = createdDate.toString().split('T')[0].split(/-/);
+                    CustomDate = created_date.split('T')[0].split(/-/);
                     createdDate = CustomDate[0]+"년 "+CustomDate[1]+"월 "+CustomDate[2]+"일";
+
                     /* 썸네일 값 여부 */
                     if(thumbnail == null || thumbnail.trim() == ''){
                         thumbnail = '/img/panda.png';
                     }
                     /* 유저 이미지 여부 */
-                    if(userImg == null || userImg.trim() == '') {
-                        userImg = '/img/userIcon.svg';
+                    if(picture == null || picture.trim() == '') {
+                        picture = '/img/userIcon.svg';
+                    }
+                    /* 해시태그 여부 */
+                    if(!hashTag) {
+                        hashTag = "";
+                    }else {
+                        hashTag += "#";
                     }
 
                     const card = document.createElement('span');
@@ -57,7 +64,8 @@ function nextPage(){
                                   </div>
                                   <!-- 몸통 -->
                                   <div class="card-body">
-                                    <span class="tag tag-pink">AnyTag</span>
+                                    <!-- 해시태그 -->
+                                    <span class="tag tag-pink">${hashTag}</span>
                                     <!-- 제목 -->
                                     <a href="/board/post/read/${id}"><h4> ${title} </h4></a>
                                     <!-- 글 내용 -->
@@ -69,17 +77,17 @@ function nextPage(){
                                       <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" class="bi bi-heart-fill" viewBox="0 0 24 24" fill="currentColor">
                                         <path fill-rule="evenodd" d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z" clip-rule="evenodd" />
                                       </svg>
-                                      <b>&nbsp;️${likes.length}</b> • ${view} views
+                                      <b>&nbsp;️${like_count}</b> • ${view} views
                                     </div>
                                     <!-- 유저 정보 -->
                                     <div class="user">
-                                      <img src="${userImg}" alt="user" />
+                                      <img src="${picture}" alt="user" />
                                       <div class="user-info">
                                         <h5>${writer}</h5>
                                         <small> ${createdDate} • <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-dots" viewBox="0 0 16 20">
                                           <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                                           <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z"/>
-                                        </svg>&nbsp;${comments.length}</small>
+                                        </svg>&nbsp;${comment_count}</small>
                                       </div>
                                     </div>
                                   </div>
