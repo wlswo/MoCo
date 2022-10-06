ac1110e484fcef30e27af78be1eebe3e4d0d2e78
JaeJinByun <byunjin11@naver.com>
2022-10-06 23:48:06 +0900

    [ADD] DOT MAP 적용 완료

---
+++ src/main/java/com/board/board/controller/earthController.java
@@ -0,0 +1,13 @@
+package com.board.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class earthController {
+
+    @GetMapping("/earth")
+    public String earthPage() {
+        return "earth/earth";
+    }
+}
