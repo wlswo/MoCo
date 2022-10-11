ea839b3376f376d0c4fb682654e2356738994ddd
JaeJinByun <byunjin11@naver.com>
2022-10-11 15:07:46 +0900

    [UPDATE] 도트맵 리스트 받아오기 + 렌더링 완료 , 도트맵 구매 완료

--- src/main/java/com/board/board/controller/earthController.java
+++ src/main/java/com/board/board/controller/earthController.java
@@ -1,13 +1,19 @@
 package com.board.board.controller;
 
+import com.board.board.config.LoginUser;
+import com.board.board.config.auth.SessionUser;
+import com.board.board.dto.DotMapDto;
 import com.board.board.service.dotmap.DotMapService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -17,11 +23,16 @@ public class earthController {
 
     @GetMapping("/earth")
     public String earthPage(Model model) {
-        Set<String> dotList = new HashSet<>();
-        for (int i = 0; i < 10; i++) {
-            dotList.add(Integer.toString(i));
-        }
+        List<DotMapDto.Response> dotList = dotMapService.getDotMapInfo();
         model.addAttribute("dotList",dotList);
         return "earth/earth";
+    }
+
+    @PostMapping("/earth/buy/{userid}")
+    public ResponseEntity buyDot(@PathVariable Long userid ,@RequestBody DotMapDto.Request dotDto,@LoginUser SessionUser sessionUser) {
+        if(userid != sessionUser.getId()) {
+            return ResponseEntity.status(400).build();
+        }
+        return ResponseEntity.ok(dotMapService.saveDot(dotDto,userid));
     }
 }
--- src/main/java/com/board/board/service/dotmap/DotMapService.java
+++ src/main/java/com/board/board/service/dotmap/DotMapService.java
@@ -1,13 +1,22 @@
 package com.board.board.service.dotmap;
 
+import com.board.board.domain.DotMap;
+import com.board.board.domain.User;
+import com.board.board.dto.CommentDto;
+import com.board.board.dto.DotMapDto;
 import com.board.board.repository.DotMapRepositoey;
 import com.board.board.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 @Service
 public class DotMapService {
     private final DotMapRepositoey dotMapRepositoey;
@@ -16,4 +25,20 @@ public class DotMapService {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     /* CREATE (도트맵 구매) */
+    @Transactional
+    public Long saveDot(DotMapDto.Request dotDto, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("회원이 존재 하지 않습니다."));
+        dotDto.setUser(user);
+
+        return dotMapRepositoey.save(dotDto.toEntity()).getId();
+    }
+
+    /* READ (구매자들 정보) */
+    @Transactional(readOnly = true)
+    public List<DotMapDto.Response> getDotMapInfo(){
+        List<DotMap> dotMapList = dotMapRepositoey.findAll();
+        List<DotMapDto.Response> dotList = dotMapList.stream().map(DotMapDto.Response::new).collect(Collectors.toList());
+        return dotList;
+    }
+
 }
