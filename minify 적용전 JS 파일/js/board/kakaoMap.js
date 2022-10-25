c3f446366546f6192b6b88bd1d6451252bab75c3
JaeJinByun <byunjin11@naver.com>
2022-10-26 04:41:32 +0900

    [ADD] kakaoMap Api 추가

---
+++ minify 적용전 JS 파일/js/board/kakaoMap.js
@@ -0,0 +1,75 @@
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places();
+
+// 키워드로 장소를 검색합니다
+if(document.getElementById('map-search')){
+    document.getElementById('map-search').addEventListener('click', () => {
+        removeMarker();
+        let location = document.getElementById('location').value;
+        ps.keywordSearch(location, placesSearchCB);
+    });
+}else {
+    ps.keywordSearch(map_marker, placesSearchCB);
+}
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    }
+}
+var markers = new Array(); //배열선언
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x)
+    });
+    markers.push(marker);
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+        map.panTo(new kakao.maps.LatLng(place.y, place.x));
+        if(document.getElementById('selected_location')) {
+            document.getElementById('selected_location').value = place.place_name;
+        }
+    });
+}
+
+// 지도 위에 표시되고 있는 마커를 모두 제거합니다
+function removeMarker() {
+    for ( var i = 0; i < markers.length; i++ ) {
+        infowindow.close();
+        markers[i].setMap(null);
+    }
+    markers = [];
+}
+
