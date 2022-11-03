7361c55165e79d473fcb3ace554fecdebf79de3d
JaeJinByun <byunjin11@naver.com>
2022-11-03 21:38:55 +0900

    [UPDATE] 주석 변경

--- src/main/java/com/board/board/controller/AwsS3Controller.java
+++ src/main/java/com/board/board/controller/AwsS3Controller.java
@@ -15,20 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 public class AwsS3Controller {
     private final AwsS3Service awsS3Service;
 
-    /**
-     * Amazon S3에 이미지 업로드
-     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
-     */
+    /** @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환 */
     @Operation(summary = "이미지 업로드 요청", description = "게시글 작성, 프로필 사진 업로드시 요청 Api 입니다.")
     @PostMapping("/image")
     public ResponseEntity uploadImage(@Parameter(description = "이미지를 multipartFile 타입으로 받습니다.") @RequestParam(value = "image", required = true) MultipartFile multipartFile) {
         return ResponseEntity.ok().body(awsS3Service.uploadImage(multipartFile));
     }
 
-    /**
-     * Amazon S3에 이미지 업로드 된 파일을 삭제
-     * @return 성공 시 200 Success
-     */
+    /** @return 성공 시 200 Success */
     @Operation(summary = "이미지 삭제 요청", description = "이미지 삭제시 요청 Api 입니다.")
     @DeleteMapping("/image")
     public ResponseEntity<Void> deleteImage(@Parameter(description = "이미지가 저장된 url 를 파라미터로 받습니다.") @RequestBody(required = true) String fileName) {
